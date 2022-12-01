b03d47b38d27b3346804b87b680fd88e48ed1c86
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-12-01 09:12:39 -0300

    Update: added pieces

---
+++ src/chess/piece/King.java
@@ -0,0 +1,15 @@
+package chess.piece;
+import chess.ChessPiece;
+import chess.Color;
+import boardgame.Board;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color){
+        super(board,color);
+    }
+    @Override
+    public String toString(){
+        return "K";
+    }
+}
---
+++ src/chess/piece/Rook.java
@@ -0,0 +1,15 @@
+package chess.piece;
+
+import chess.ChessPiece;
+import chess.Color;
+import boardgame.Board;
+
+public class Rook extends ChessPiece {
+    public Rook(Board board, Color color){
+        super(board,color);
+    }
+    @Override
+    public String toString(){
+        return "R";
+    }
+}
